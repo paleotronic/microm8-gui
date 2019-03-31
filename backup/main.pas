ab6c8b5c123c5d6158f7cb56e3d3bb824c43966f
April Ayres-Griffiths <aag6581@gmail.com>
2019-03-31 19:03:43 +1100

    disk and keyboard fixes

--- backup/main.pas
+++ backup/main.pas
@@ -21,26 +21,38 @@ type
     BackdropImage: TImage;
     ImageList1: TImageList;
     MainMenu1: TMainMenu;
-    Memo1: TMemo;
     MenuItem1: TMenuItem;
     MenuItem10: TMenuItem;
     MenuItem11: TMenuItem;
     MenuItem12: TMenuItem;
     MenuItem13: TMenuItem;
+    miIntLOGO: TMenuItem;
+    miIntINT: TMenuItem;
+    miIntFP: TMenuItem;
     MenuItem2: TMenuItem;
     embedPanel: TPanel;
+    MenuItem3: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    miSPEject: TMenuItem;
+    miSPFile: TMenuItem;
+    miFileCat: TMenuItem;
+    miD2File: TMenuItem;
+    miD1File: TMenuItem;
     menuRebootVM: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
     MenuItem6: TMenuItem;
     MenuItem7: TMenuItem;
-    MenuItem8: TMenuItem;
-    MenuItem9: TMenuItem;
+    miD1Blank: TMenuItem;
+    miD2Blank: TMenuItem;
     DiskMenu: TPopupMenu;
+    odDiskImages: TOpenDialog;
     StatusBar1: TStatusBar;
     hc: TFPHttpClient;
     CheckTimer: TTimer;
     ToolBar1: TToolBar;
+    procedure BackdropImageClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure CheckTimerTimer(Sender: TObject);
     procedure FormActivate(Sender: TObject);
@@ -58,6 +70,16 @@ type
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure Freeze;
+    procedure miD1BlankClick(Sender: TObject);
+    procedure miD1FileClick(Sender: TObject);
+    procedure miD2BlankClick(Sender: TObject);
+    procedure miD2FileClick(Sender: TObject);
+    procedure miFileCatClick(Sender: TObject);
+    procedure miIntFPClick(Sender: TObject);
+    procedure miIntINTClick(Sender: TObject);
+    procedure miIntLOGOClick(Sender: TObject);
+    procedure miSPEjectClick(Sender: TObject);
+    procedure miSPFileClick(Sender: TObject);
     procedure UnFreeze;
     procedure HideM8;
     procedure menuRebootVMClick(Sender: TObject);
@@ -66,12 +88,13 @@ type
     procedure ReposWindow;
     procedure RepaintWindow;
     function GetTitleOfActiveWindow: string;
-    procedure SendKey(key: Char; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
+    procedure SendKey(key: Integer; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
     procedure tbDisk1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure tbDisk2MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure ToolButton5Click(Sender: TObject);
+    procedure InsertDisk(Filename: string; Drive: integer);
   private
     lx, ly, lw, lh: integer;
     lastShowTime: TDateTime;
@@ -106,7 +129,7 @@ begin
      if MillisecondsBetween(Now(), lastHideTime) < 1000 then
       exit;
      ShowM8;
-     Memo1.Lines.Add('app is activating');
+     //Memo1.Lines.Add('app is activating');
 end;
 
 procedure TGUIForm.AppDeactivate(Sender: TObject);
@@ -116,7 +139,7 @@ begin
      RepaintWindow;
      HideM8;
      StatusBar1.SimpleText := 'App has lost focus!';
-     Memo1.Lines.Add('app is deactivating');
+     //Memo1.Lines.Add('app is deactivating');
 end;
 
 procedure TGUIForm.RebootVM;
@@ -129,6 +152,74 @@ begin
        self.hc.Get('http://localhost:38911/api/control/window/freeze');
 end;
 
+procedure TGUIForm.miD1BlankClick(Sender: TObject);
+begin
+        self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/0');
+end;
+
+procedure TGUIForm.miD1FileClick(Sender: TObject);
+begin
+     RepaintWindow;
+     HideM8;
+     if odDiskImages.Execute then
+     begin
+          InsertDisk( 'local:'+odDiskImages.Filename, 0 );
+     end;
+     ShowM8;
+end;
+
+procedure TGUIForm.miD2BlankClick(Sender: TObject);
+begin
+         self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/1');
+end;
+
+procedure TGUIForm.miD2FileClick(Sender: TObject);
+begin
+  RepaintWindow;
+  HideM8;
+  if odDiskImages.Execute then
+  begin
+       InsertDisk( 'local:'+odDiskImages.Filename, 1 );
+  end;
+  ShowM8;
+end;
+
+procedure TGUIForm.miFileCatClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/system/catalog');
+end;
+
+procedure TGUIForm.miIntFPClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/fp');
+end;
+
+procedure TGUIForm.miIntINTClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/int');
+end;
+
+procedure TGUIForm.miIntLOGOClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/logo');
+end;
+
+procedure TGUIForm.miSPEjectClick(Sender: TObject);
+begin
+      self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/2');
+end;
+
+procedure TGUIForm.miSPFileClick(Sender: TObject);
+begin
+  RepaintWindow;
+  HideM8;
+  if odDiskImages.Execute then
+  begin
+       InsertDisk( 'local:'+odDiskImages.Filename, 2 );
+  end;
+  ShowM8;
+end;
+
 procedure TGUIForm.UnFreeze;
 begin
        self.hc.Get('http://localhost:38911/api/control/window/unfreeze');
@@ -138,7 +229,7 @@ procedure TGUIForm.HideM8;
 begin
        if hidden then
          exit;
-       Memo1.Lines.Add('hiding m8 window');
+       //Memo1.Lines.Add('hiding m8 window');
        self.hc.Get('http://localhost:38911/api/control/window/hide');
        lastHideTime := Now();
        hidden := true;
@@ -153,7 +244,7 @@ procedure TGUIForm.ShowM8;
 begin
        if not hidden then
           exit;
-       Memo1.Lines.Add('showing m8 window');
+       //Memo1.Lines.Add('showing m8 window');
        self.hc.Get('http://localhost:38911/api/control/window/show');
        lastShowTime := Now();
        hidden := false;
@@ -211,15 +302,15 @@ begin
      Respo.Destroy;
 end;
 
-procedure TGUIForm.SendKey(key: Char; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
+procedure TGUIForm.SendKey(key: Integer; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
 var
   json, S: string;
   Respo: TStringStream;
 begin
-     if key = Char(8) then
-        key := Char(127);
+     if key = 8 then
+        key := 127;
 
-     json := '{"key":' + IntToStr(Ord(key)) +
+     json := '{"key":' + IntToStr(key) +
              ',"scancode":'+IntToStr(ScanCode) +
              ',"action":'+IntToStr(KeyAction) +
              ',"modifiers":'+IntToStr(Mods) +
@@ -228,6 +319,25 @@ begin
      self.hc.SimpleFormPost('http://localhost:38911/api/control/input/keyevent',json,Respo);
      S := Respo.DataString;
      self.StatusBar1.SimpleText:=S;
+     Respo.Destroy;
+end;
+
+procedure TGUIForm.InsertDisk(Filename: string; Drive: integer);
+var
+  json, S: string;
+  Respo: TStringStream;
+  f: TReplaceFlags;
+begin
+     f := [rfReplaceAll];
+     Filename := StringReplace( Filename, '\', '/', f);
+
+     json := '{"filename":"' + Filename +
+             '","drive":'+IntToStr(Drive) +
+             '}';
+     Respo := TStringStream.Create('');
+     self.hc.SimpleFormPost('http://localhost:38911/api/control/hardware/disk/insert',json,Respo);
+     S := Respo.DataString;
+     self.StatusBar1.SimpleText:=json;
      Respo.Destroy;
 end;
 
@@ -267,6 +377,11 @@ end;
 procedure TGUIForm.Button1Click(Sender: TObject);
 begin
      self.ReposWindow;
+end;
+
+procedure TGUIForm.BackdropImageClick(Sender: TObject);
+begin
+
 end;
 
 {$IFDEF WINDOWS}
@@ -334,8 +449,112 @@ begin
   HideM8;
 end;
 
-procedure TGUIForm.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
-  );
+function MapKeyCode( Key: Word; Shift: TShiftState ): integer;
+const
+  	CSR_LEFT  = $e000;
+	CSR_RIGHT = $e001;
+	CSR_UP    = $e002;
+	CSR_DOWN  = $e003;
+        CTRL_A = $e041;
+begin
+  case Key of
+  219:
+        if ssShift in Shift then
+       Result := Ord('{')
+      else
+        Result := Ord('[');
+  221:
+              if ssShift in Shift then
+       Result := Ord('}')
+      else
+        Result := Ord(']');
+  222:
+              if ssShift in Shift then
+       Result := Ord('"')
+      else
+        Result := Ord('''');
+  186:
+              if ssShift in Shift then
+       Result := Ord(':')
+      else
+        Result := Ord(';');
+  220:
+              if ssShift in Shift then
+       Result := Ord('|')
+      else
+        Result := Ord('\');
+  191:
+              if ssShift in Shift then
+       Result := Ord('?')
+      else
+        Result := Ord('/');
+  190:
+              if ssShift in Shift then
+       Result := Ord('>')
+      else
+        Result := Ord('.');
+  188:
+              if ssShift in Shift then
+       Result := Ord('<')
+      else
+        Result := Ord(',');
+  187:
+              if ssShift in Shift then
+       Result := Ord('+')
+      else
+        Result := Ord('=');
+  189:
+              if ssShift in Shift then
+       Result := Ord('_')
+      else
+        Result := Ord('-');
+  192:
+      if ssShift in Shift then
+       Result := Ord('~')
+      else
+        Result := Ord('`');
+  VK_Up: Result := CSR_UP;
+  VK_Down: Result := CSR_DOWN;
+  VK_Left: Result := CSR_LEFT;
+  VK_Right: Result := CSR_RIGHT;
+  VK_0..VK_9:
+  begin
+      if ssShift in Shift then
+      case Key of
+       VK_0: Result := Ord(')');
+       VK_9: Result := Ord('(');
+       VK_8: Result := Ord('*');
+       VK_7: Result := Ord('&');
+       VK_6: Result := Ord('^');
+       VK_5: Result := Ord('%');
+       VK_4: Result := Ord('$');
+       VK_3: Result := Ord('#');
+       VK_2: Result := Ord('@');
+       VK_1: Result := Ord('!');
+      end
+      else
+          Result := Integer(Key);
+  end;
+  VK_A..VK_Z:
+  begin
+             if ssShift in Shift then
+              Result := Integer(Key)
+             else if ssCtrl in Shift then
+             begin
+                if Key = 67 then
+                  Result = 3
+                else
+                  Result := CTRL_A + Integer(Key-64);
+             end
+             else
+               Result := Integer(Key)+32;
+  end
+  else
+    result := Integer(Key)
+  end;
+end;
+
+function MapShiftState( Key: Word; Shift: TShiftState ): integer;
 var
   m: Integer;
 begin
@@ -346,26 +565,24 @@ begin
      m := m or 2;
   if ssAlt in Shift then
      m := m or 4;
-  SendKey( Char(Key), 0, 1, m );
+  result := m;
+end;
+
+procedure TGUIForm.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
+  );
+begin
+  SendKey( MapKeyCode(Key, Shift), 0, 1, MapShiftState(Key, Shift) );
 end;
 
 procedure TGUIForm.FormKeyPress(Sender: TObject; var Key: char);
 begin
-  SendKey( Key, 0, 1, 0 );
+  //SendKey( Integer(Ord(Key)), 0, 1, 0 );
 end;
 
 procedure TGUIForm.FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
-var
-  m: Integer;
 begin
-  m := 0;
-  if ssShift in Shift then
-     m := m or 1;
-  if ssCtrl in Shift then
-     m := m or 2;
-  if ssAlt in Shift then
-     m := m or 4;
-  SendKey( Char(Key), 0, 0, m );
+  SendKey( MapKeyCode(Key, Shift), 0, 0, MapShiftState(Key, Shift) );
+  StatusBar1.SimpleText := 'keycode ' + IntToStr(Key);
 end;
 
 procedure TGUIForm.FormResize(Sender: TObject);
@@ -387,7 +604,7 @@ end;
 
 procedure TGUIForm.FormWindowStateChange(Sender: TObject);
 begin
-  Memo1.Lines.Add('window state has changed');
+  //Memo1.Lines.Add('window state has changed');
   if WindowState = wsMinimized then
      HideM8
   else
--- main.pas
+++ main.pas
@@ -26,20 +26,33 @@ type
     MenuItem11: TMenuItem;
     MenuItem12: TMenuItem;
     MenuItem13: TMenuItem;
+    miIntLOGO: TMenuItem;
+    miIntINT: TMenuItem;
+    miIntFP: TMenuItem;
     MenuItem2: TMenuItem;
     embedPanel: TPanel;
+    MenuItem3: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    miSPEject: TMenuItem;
+    miSPFile: TMenuItem;
+    miFileCat: TMenuItem;
+    miD2File: TMenuItem;
+    miD1File: TMenuItem;
     menuRebootVM: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
     MenuItem6: TMenuItem;
     MenuItem7: TMenuItem;
-    MenuItem8: TMenuItem;
-    MenuItem9: TMenuItem;
+    miD1Blank: TMenuItem;
+    miD2Blank: TMenuItem;
     DiskMenu: TPopupMenu;
+    odDiskImages: TOpenDialog;
     StatusBar1: TStatusBar;
     hc: TFPHttpClient;
     CheckTimer: TTimer;
     ToolBar1: TToolBar;
+    procedure BackdropImageClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure CheckTimerTimer(Sender: TObject);
     procedure FormActivate(Sender: TObject);
@@ -57,6 +70,16 @@ type
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure Freeze;
+    procedure miD1BlankClick(Sender: TObject);
+    procedure miD1FileClick(Sender: TObject);
+    procedure miD2BlankClick(Sender: TObject);
+    procedure miD2FileClick(Sender: TObject);
+    procedure miFileCatClick(Sender: TObject);
+    procedure miIntFPClick(Sender: TObject);
+    procedure miIntINTClick(Sender: TObject);
+    procedure miIntLOGOClick(Sender: TObject);
+    procedure miSPEjectClick(Sender: TObject);
+    procedure miSPFileClick(Sender: TObject);
     procedure UnFreeze;
     procedure HideM8;
     procedure menuRebootVMClick(Sender: TObject);
@@ -65,12 +88,13 @@ type
     procedure ReposWindow;
     procedure RepaintWindow;
     function GetTitleOfActiveWindow: string;
-    procedure SendKey(key: Char; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
+    procedure SendKey(key: Integer; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
     procedure tbDisk1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure tbDisk2MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure ToolButton5Click(Sender: TObject);
+    procedure InsertDisk(Filename: string; Drive: integer);
   private
     lx, ly, lw, lh: integer;
     lastShowTime: TDateTime;
@@ -126,6 +150,74 @@ end;
 procedure TGUIForm.Freeze;
 begin
        self.hc.Get('http://localhost:38911/api/control/window/freeze');
+end;
+
+procedure TGUIForm.miD1BlankClick(Sender: TObject);
+begin
+        self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/0');
+end;
+
+procedure TGUIForm.miD1FileClick(Sender: TObject);
+begin
+     RepaintWindow;
+     HideM8;
+     if odDiskImages.Execute then
+     begin
+          InsertDisk( 'local:'+odDiskImages.Filename, 0 );
+     end;
+     ShowM8;
+end;
+
+procedure TGUIForm.miD2BlankClick(Sender: TObject);
+begin
+         self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/1');
+end;
+
+procedure TGUIForm.miD2FileClick(Sender: TObject);
+begin
+  RepaintWindow;
+  HideM8;
+  if odDiskImages.Execute then
+  begin
+       InsertDisk( 'local:'+odDiskImages.Filename, 1 );
+  end;
+  ShowM8;
+end;
+
+procedure TGUIForm.miFileCatClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/system/catalog');
+end;
+
+procedure TGUIForm.miIntFPClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/fp');
+end;
+
+procedure TGUIForm.miIntINTClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/int');
+end;
+
+procedure TGUIForm.miIntLOGOClick(Sender: TObject);
+begin
+    self.hc.Get('http://localhost:38911/api/control/interpreter/logo');
+end;
+
+procedure TGUIForm.miSPEjectClick(Sender: TObject);
+begin
+      self.hc.Get('http://localhost:38911/api/control/hardware/disk/eject/2');
+end;
+
+procedure TGUIForm.miSPFileClick(Sender: TObject);
+begin
+  RepaintWindow;
+  HideM8;
+  if odDiskImages.Execute then
+  begin
+       InsertDisk( 'local:'+odDiskImages.Filename, 2 );
+  end;
+  ShowM8;
 end;
 
 procedure TGUIForm.UnFreeze;
@@ -210,15 +302,15 @@ begin
      Respo.Destroy;
 end;
 
-procedure TGUIForm.SendKey(key: Char; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
+procedure TGUIForm.SendKey(key: Integer; ScanCode: Integer; KeyAction: Integer; Mods: Integer);
 var
   json, S: string;
   Respo: TStringStream;
 begin
-     if key = Char(8) then
-        key := Char(127);
+     if key = 8 then
+        key := 127;
 
-     json := '{"key":' + IntToStr(Ord(key)) +
+     json := '{"key":' + IntToStr(key) +
              ',"scancode":'+IntToStr(ScanCode) +
              ',"action":'+IntToStr(KeyAction) +
              ',"modifiers":'+IntToStr(Mods) +
@@ -227,6 +319,25 @@ begin
      self.hc.SimpleFormPost('http://localhost:38911/api/control/input/keyevent',json,Respo);
      S := Respo.DataString;
      self.StatusBar1.SimpleText:=S;
+     Respo.Destroy;
+end;
+
+procedure TGUIForm.InsertDisk(Filename: string; Drive: integer);
+var
+  json, S: string;
+  Respo: TStringStream;
+  f: TReplaceFlags;
+begin
+     f := [rfReplaceAll];
+     Filename := StringReplace( Filename, '\', '/', f);
+
+     json := '{"filename":"' + Filename +
+             '","drive":'+IntToStr(Drive) +
+             '}';
+     Respo := TStringStream.Create('');
+     self.hc.SimpleFormPost('http://localhost:38911/api/control/hardware/disk/insert',json,Respo);
+     S := Respo.DataString;
+     self.StatusBar1.SimpleText:=json;
      Respo.Destroy;
 end;
 
@@ -266,6 +377,11 @@ end;
 procedure TGUIForm.Button1Click(Sender: TObject);
 begin
      self.ReposWindow;
+end;
+
+procedure TGUIForm.BackdropImageClick(Sender: TObject);
+begin
+
 end;
 
 {$IFDEF WINDOWS}
@@ -333,8 +449,112 @@ begin
   HideM8;
 end;
 
-procedure TGUIForm.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
-  );
+function MapKeyCode( Key: Word; Shift: TShiftState ): integer;
+const
+  	CSR_LEFT  = $e000;
+	CSR_RIGHT = $e001;
+	CSR_UP    = $e002;
+	CSR_DOWN  = $e003;
+        CTRL_A = $e041;
+begin
+  case Key of
+  219:
+        if ssShift in Shift then
+       Result := Ord('{')
+      else
+        Result := Ord('[');
+  221:
+              if ssShift in Shift then
+       Result := Ord('}')
+      else
+        Result := Ord(']');
+  222:
+              if ssShift in Shift then
+       Result := Ord('"')
+      else
+        Result := Ord('''');
+  186:
+              if ssShift in Shift then
+       Result := Ord(':')
+      else
+        Result := Ord(';');
+  220:
+              if ssShift in Shift then
+       Result := Ord('|')
+      else
+        Result := Ord('\');
+  191:
+              if ssShift in Shift then
+       Result := Ord('?')
+      else
+        Result := Ord('/');
+  190:
+              if ssShift in Shift then
+       Result := Ord('>')
+      else
+        Result := Ord('.');
+  188:
+              if ssShift in Shift then
+       Result := Ord('<')
+      else
+        Result := Ord(',');
+  187:
+              if ssShift in Shift then
+       Result := Ord('+')
+      else
+        Result := Ord('=');
+  189:
+              if ssShift in Shift then
+       Result := Ord('_')
+      else
+        Result := Ord('-');
+  192:
+      if ssShift in Shift then
+       Result := Ord('~')
+      else
+        Result := Ord('`');
+  VK_Up: Result := CSR_UP;
+  VK_Down: Result := CSR_DOWN;
+  VK_Left: Result := CSR_LEFT;
+  VK_Right: Result := CSR_RIGHT;
+  VK_0..VK_9:
+  begin
+      if ssShift in Shift then
+      case Key of
+       VK_0: Result := Ord(')');
+       VK_9: Result := Ord('(');
+       VK_8: Result := Ord('*');
+       VK_7: Result := Ord('&');
+       VK_6: Result := Ord('^');
+       VK_5: Result := Ord('%');
+       VK_4: Result := Ord('$');
+       VK_3: Result := Ord('#');
+       VK_2: Result := Ord('@');
+       VK_1: Result := Ord('!');
+      end
+      else
+          Result := Integer(Key);
+  end;
+  VK_A..VK_Z:
+  begin
+             if ssShift in Shift then
+              Result := Integer(Key)
+             else if ssCtrl in Shift then
+             begin
+                if Key = 67 then
+                  Result := 3
+                else
+                  Result := CTRL_A + Integer(Key-64);
+             end
+             else
+               Result := Integer(Key)+32;
+  end
+  else
+    result := Integer(Key)
+  end;
+end;
+
+function MapShiftState( Key: Word; Shift: TShiftState ): integer;
 var
   m: Integer;
 begin
@@ -345,26 +565,24 @@ begin
      m := m or 2;
   if ssAlt in Shift then
      m := m or 4;
-  SendKey( Char(Key), 0, 1, m );
+  result := m;
+end;
+
+procedure TGUIForm.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
+  );
+begin
+  SendKey( MapKeyCode(Key, Shift), 0, 1, MapShiftState(Key, Shift) );
 end;
 
 procedure TGUIForm.FormKeyPress(Sender: TObject; var Key: char);
 begin
-  SendKey( Key, 0, 1, 0 );
+  //SendKey( Integer(Ord(Key)), 0, 1, 0 );
 end;
 
 procedure TGUIForm.FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
-var
-  m: Integer;
 begin
-  m := 0;
-  if ssShift in Shift then
-     m := m or 1;
-  if ssCtrl in Shift then
-     m := m or 2;
-  if ssAlt in Shift then
-     m := m or 4;
-  SendKey( Char(Key), 0, 0, m );
+  SendKey( MapKeyCode(Key, Shift), 0, 0, MapShiftState(Key, Shift) );
+  StatusBar1.SimpleText := 'keycode ' + IntToStr(Key);
 end;
 
 procedure TGUIForm.FormResize(Sender: TObject);
